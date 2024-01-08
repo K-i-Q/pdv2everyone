6c50ea647cf824136ca265ef4f4c29b25cd1928c
Carlos <carlos.oliveira@inteliger.com.br>
2024-01-08 08:49:55 -0300

    - navbar menu

---
+++ src/components/auth/user-button.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { ExitIcon } from "@radix-ui/react-icons";
+import { FaUser } from "react-icons/fa";
+import { LogoutButton } from "./logout-button";
+
+const UserButton = () => {
+    const user = useCurrentUser();
+
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger>
+                <Avatar>
+                    <AvatarImage src={user?.image || ""} />
+                    <AvatarFallback className="bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-sky-400 to-blue-800">
+                        <FaUser className="text-white" />
+                    </AvatarFallback>
+                </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-40" align="end">
+                <LogoutButton>
+                    <DropdownMenuItem>
+                        <ExitIcon className="w-4 h-4 mr-2" />
+                        Sair
+                    </DropdownMenuItem>
+                </LogoutButton>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    )
+}
+
+export default UserButton;
