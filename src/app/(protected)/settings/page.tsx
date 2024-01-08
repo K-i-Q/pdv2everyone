ae4362f3e7ddcde501225b12c6b2a9b09bfde343
Carlos <carlos.oliveira@inteliger.com.br>
2024-01-08 09:53:24 -0300

    - server component

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
-import { LogoutButton } from "@/components/auth/logout-button";
 import { useCurrentUser } from "@/hooks/use-current-user";
 const SettingsPage = () => {
     const currentUser = useCurrentUser();
 
     return (
         <div className="bg-white p-10 rounded-xl">
-            <LogoutButton>
-                Sair
-            </LogoutButton>
+            Settings page
         </div>
     )
 }
