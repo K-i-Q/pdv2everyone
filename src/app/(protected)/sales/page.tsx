32bf46aecd1459c66454fd9a63e34d75849da58e
Carlos <carlos.oliveira@inteliger.com.br>
2024-01-31 08:52:36 -0300

    - alteração do schema do prisma - função para criar colaboradores mock - etapas para criar e atualizar a venda

--- src/app/(protected)/sales/page.tsx
+++ src/app/(protected)/sales/page.tsx
@@ -1,11 +1,17 @@
 "use client"
 import { getEmployees } from "@/actions/employees";
 import { getProducts } from "@/actions/products";
-import { createSale } from "@/actions/sales";
+import { createEmployees, createSale, saveCustomer } from "@/actions/sales";
 import { getServices } from "@/actions/services";
-import { Card, CardHeader } from "@/components/ui/card";
+import LoadingAnimation from "@/components/custom/LoadingAnimation";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { formatPriceBRL } from "@/utils/mask";
 import { Employee, Product, Service } from "@prisma/client";
 import { useEffect, useState, useTransition } from "react";
+import { GrLinkNext } from "react-icons/gr";
 import { toast } from "sonner";
 
 const SalesPage = () => {
@@ -13,6 +19,8 @@ const SalesPage = () => {
     const [services, setServices] = useState<Service[] | undefined>();
     const [products, setProducts] = useState<Product[] | undefined>();
     const [totalPrice, setTotalPrice] = useState<Number | undefined>();
+    const [saleId, setSaleId] = useState<string>('');
+    const [customerDocument, setCustomerDocument] = useState<string>('');
     const [isPending, startTransition] = useTransition();
     const colors = ['#b89d04', '#d40423', '#0250cf']; // Exemplo de cores
     const [currentStep, setCurrentStep] = useState(0);
@@ -67,10 +75,45 @@ const SalesPage = () => {
                 if (data.success) {
                     setCurrentStep(currentStep + 1);
                     setSlideDirection("left-to-right");
+                    setSaleId(data.saleId)
                 }
             }).catch(() => toast.error("Algo deu errado"))
         })
     }
+
+    const handleSaveCustomer = (saleId: string, customerDocument: string) => {
+        if (!saleId || !customerDocument) {
+            return
+        }
+
+        startTransition(() => {
+            saveCustomer(saleId, customerDocument).then((data) => {
+                if (data?.error) {
+                    toast.error(data.error);
+                }
+
+                if (data?.success) {
+                    setCurrentStep(currentStep + 1);
+                    setSlideDirection("left-to-right");
+                    console.log('services', services)
+                }
+            })
+        })
+    }
+
+    const handleCreateEmployees = () => {
+        startTransition(() => {
+            createEmployees().then((data) => {
+                if (data?.success) {
+                    toast.success(data.success)
+                }
+            })
+        })
+    }
+
+    const handleInputCustomerChange = (event: any) => {
+        setCustomerDocument(event.target.value);
+    };
 
 
     return (
@@ -99,12 +142,62 @@ const SalesPage = () => {
                             </div>
                         )}
                         {currentStep === 1 && (
-                            <div className={`alguma-outro-div-com-conteúdo ${slideDirection === "left-to-right" ? "slide-left" : "slide-right"}`}>
-                               DEUBOA
+                            <Card className={`${slideDirection === "left-to-right" ? "slide-left" : "slide-right"}`}>
+                                <CardHeader>
+                                    <Label>CPF</Label>
+                                </CardHeader>
+                                <CardContent>
+                                    <Input placeholder="Digite aqui o CPF do cliente" onChange={handleInputCustomerChange} value={customerDocument} />
+                                </CardContent>
+                                <CardFooter className="flex-row-reverse">
+                                    <Button
+                                        className="gap-x-3"
+                                        disabled={isPending}
+                                        onClick={() => handleSaveCustomer(saleId, customerDocument)}
+                                    >
+                                        Próximo
+                                        <GrLinkNext />
+                                    </Button>
+                                </CardFooter>
+                            </Card>
+                        )}
+                        {currentStep === 2 && (
+                            <div className="grid grid-cols-4 gap-x-4">
+                                <>
+                                    {
+                                        services?.map((service) => (
+                                            <Card key={service.id} className={`hover:bg-slate-700 cursor-pointer ${slideDirection === "left-to-right" ? "slide-left" : "slide-right"}`}>
+                                                <CardHeader>
+                                                    <Label>{service.name}</Label>
+                                                </CardHeader>
+                                                <CardContent>
+                                                    {service.description}
+                                                </CardContent>
+                                                <CardFooter>
+                                                    {formatPriceBRL(service.salePrice)}
+                                                </CardFooter>
+                                            </Card>
+                                        ))
+                                    }
+                                </>
                             </div>
+
                         )}
                     </>
                 )}
+                {
+                    !employees && (
+                        <LoadingAnimation />
+                    )
+                }
+                {
+                    (!employees || employees?.length === 0) && (
+                        <>
+                            <Button disabled={isPending} onClick={handleCreateEmployees}>Criar funcionarios</Button>
+                        </>
+                    )
+                }
+
             </>
         </div>
     )
